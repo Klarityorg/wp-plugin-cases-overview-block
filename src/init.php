562043b70c3be1084d3e15798eb8248dde701d8b
Bruno Perel <bruno@klarity.org>
2019-03-25 11:04:53 +0100

    Prevent a few caching issues

--- src/init.php
+++ src/init.php
@@ -67,7 +67,7 @@ function render_klarity_cases_overview_list($attributes) {
     'case_overview_see_all_see_less-js',
     plugins_url('/src/block/see-all-see-less.js', __DIR__),
     [],
-    true
+    filemtime(plugins_url('/src/block/see-all-see-less.js', __DIR__))
   );
 
   global $post;
